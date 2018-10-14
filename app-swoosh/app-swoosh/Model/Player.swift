ba340021d142a805ab3252061b1bb0a1fbe1d91c
exxe <exxe06@MarcTerencesMBP.fritz.box>
2018-10-14 17:18:00 +0200

    d2 s4 lecture 35

---
+++ app-swoosh/app-swoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by exxe on 14.10.18.
+//  Copyright © 2018 exxe. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLevel: String?
+}
